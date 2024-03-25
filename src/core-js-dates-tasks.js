f55eee06d61f4ef73f086b8ae8663667d7716593
Alex <fornikaras7@gmail.com>
2024-03-25 18:00:24 +0500

    feat: finish getNextFriday13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -207,11 +207,26 @@ function getWeekNumberByDate(/* date */) {
  * @return {Date} - The date of the next Friday the 13th.
  *
  * @example:
- * Date(2024, 0, 13) => Date(2024, 8, 13)
+ *   => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let year = new Date(date).getFullYear();
+  let month = new Date(date).getMonth();
+  let isFind = false;
+  while (!isFind) {
+    const friday = new Date(year, month, 13);
+    if (friday.getDay() === 5) {
+      isFind = true;
+      return friday;
+    }
+    month += 1;
+    if (month === 12) {
+      year += 1;
+      month = 0;
+    }
+  }
+  return false;
 }
 
 /**
@@ -251,8 +266,16 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period /* countWorkDays, countOffDays */) {
+  return new Date(period.end);
+  // const schedule = [];
+  // const dateRange = new Date(start).getDate() - new Date(end).getDate();
+  // const date = new Date(start);
+  // date.setDate(date.getDate() + countOffDays);
+  // const day = date.getDate().toString().padStart(2, '0');
+  // const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  // const year = date.getFullYear();
+  // return `${day}-${month}-${year}`;
 }
 
 /**
