c68d7d5877decec584a8fd4fa045186002752d1a
Alex <fornikaras7@gmail.com>
2024-03-25 23:54:51 +0500

    trying to find wrong answer

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -217,13 +217,14 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(date) {
-  const firstDayOfYear = new Date(date.getUTCFullYear(), 0);
-  const dayMilliseconds = 86400000;
-  return Math.ceil(
-    ((date - firstDayOfYear) / dayMilliseconds + firstDayOfYear.getDay() + 1) /
-      7
-  );
+function getWeekNumberByDate(/* date */) {
+  // const firstDayOfYear = new Date(date.getUTCFullYear(), 0);
+  // const dayMilliseconds = 86400000;
+  // return Math.ceil(
+  //   ((date - firstDayOfYear) / dayMilliseconds + firstDayOfYear.getDay() + 1) /
+  //     7
+  // );
+  throw new Error('Not implemented');
 }
 
 /**
