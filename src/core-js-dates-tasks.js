01a79ee803349ae39e0b281e94d9024490e5c3ee
Alex <fornikaras7@gmail.com>
2024-03-25 20:34:26 +0500

    fix

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -238,12 +238,12 @@ function getWeekNumberByDate(date) {
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
 function getNextFridayThe13th(date) {
-  let year = new Date(date).getUTCFullYear();
-  let month = new Date(date).getUTCMonth();
+  let year = new Date(date).getFullYear();
+  let month = new Date(date).getMonth();
   let isFind = false;
   while (!isFind) {
     const friday = new Date(year, month, 13);
-    if (friday.getUTCDay() === 5) {
+    if (friday.getDay() === 5) {
       isFind = true;
       return friday;
     }
