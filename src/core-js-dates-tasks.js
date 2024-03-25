ed8312417f523727d16682077852fff045495e90
Alex <fornikaras7@gmail.com>
2024-03-25 20:50:58 +0500

    fix: try to fix

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -294,7 +294,7 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
 function getWorkSchedule(/* period ,countWorkDays, countOffDays */) {
-  throw new Error();
+  throw new Error('Not implemented');
 }
 
 /**
